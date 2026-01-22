a2176ae57cf30a3f5febb3c635b0432791712341
xingysh <chenrr@dsp.ac.cn>
2026-01-22 16:17:17 +0800

    change default sort type

--- client/partials/NavBar.vue
+++ client/partials/NavBar.vue
@@ -65,7 +65,7 @@ const menuItems = [
         name: "search",
         query: {
           [params.searchTerm]: "*",
-          [params.sortBy]: searchSortOptions.title,
+          [params.sortBy]: searchSortOptions.lastModified,
         },
       }),
   },
